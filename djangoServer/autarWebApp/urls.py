09a1fd9cc89d6eaec93533c5428850da044a52a4
Robert1191 <robert01011991@gmail.com>
2018-05-08 23:56:20 +1200

    sss

---
+++ djangoServer/autarWebApp/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    url('', views.index, name='index'),
+]
+
+urlpatterns += staticfiles_urlpatterns()
