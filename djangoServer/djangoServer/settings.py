6e1b0e04a82d9f0eeebcb83d6dd23199e5a9015c
Robert1191 <robert01011991@gmail.com>
2018-05-09 14:38:28 +1200

    a

--- djangoServer/djangoServer/settings.py
+++ djangoServer/djangoServer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'clh(igb@+p&#)1g02$8%2%+j&izuc&jmyd%s)c7mjcxyhfihnu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
